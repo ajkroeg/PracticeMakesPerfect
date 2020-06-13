146c4aa7d77e798ea2d75d87898a3de2c0833985
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2020-06-13 13:05:44 -0400

    Initial commit

---
+++ ModInit.cs
@@ -0,0 +1,38 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace PracticeMakesPerfect
+{
+
+    public static class ModInit
+    {
+        public static PracticeMakesPerfectSettings Settings = new PracticeMakesPerfectSettings();
+        public const string HarmonyPackage = "us.tbone.PracticeMakesPerfect";
+        public static void Init(string directory, string settingsJSON)
+        {
+            try
+            {
+                ModInit.Settings = JsonConvert.DeserializeObject<PracticeMakesPerfectSettings>(settingsJSON);
+            }
+            catch (Exception)
+            {
+
+                ModInit.Settings = new PracticeMakesPerfectSettings();
+            }
+            var harmony = HarmonyInstance.Create(HarmonyPackage);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+    public class PracticeMakesPerfectSettings
+    {    
+        public int bonusXP_MechKills = 100;
+        public int bonusXP_OtherKills = 50;
+        public float bonusXP_StrDamageMult = 0.0f;
+        public float bonusXP_ArmDamageMult = 0.0f;
+        public int bonusXP_CAP = -1;
+
+    }
+}
---
+++ PracticeMakesPerfect.cs
@@ -0,0 +1,61 @@
+﻿using Harmony;
+using BattleTech;
+using BattleTech.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PracticeMakesPerfect
+{
+    public class PracticeMakesPerfect
+    {
+        [HarmonyPatch(typeof(AAR_UnitStatusWidget), "FillInPilotData")]
+        public static class FillInPilotData_Patch
+
+        {
+            public static bool Prefix(AAR_UnitStatusWidget __instance, ref int xpEarned, UnitResult ___UnitData)
+            {
+                
+                int mechsKilled = ___UnitData.pilot.MechsKilled;
+                int newXP = 0;
+                for (int i = 0; i < mechsKilled; i++)
+                {
+                    newXP += ModInit.Settings.bonusXP_MechKills;
+                }
+                int othersKilled = ___UnitData.pilot.OthersKilled;
+                for (int j = 0; j < othersKilled; j++)
+                {
+                    newXP += ModInit.Settings.bonusXP_OtherKills;
+                }
+                newXP += Mathf.RoundToInt((___UnitData.pilot.StructureDamageInflicted * ModInit.Settings.bonusXP_StrDamageMult) + (___UnitData.pilot.ArmorDamageInflicted * ModInit.Settings.bonusXP_ArmDamageMult));
+
+                if (newXP > ModInit.Settings.bonusXP_CAP && ModInit.Settings.bonusXP_CAP > 0)
+                {
+                    newXP = ModInit.Settings.bonusXP_CAP;
+                }
+                ___UnitData.pilot.AddExperience(0, "FromKillsOrDmg", newXP);
+
+                xpEarned += newXP;
+                return true;
+            }
+        }
+        [HarmonyPatch(typeof(SimGameState), "ResolveCompleteContract")]
+        [HarmonyPriority(Priority.Low)]
+        public static class ResolveCompleteContract_Patch
+
+        {
+            public static void Postfix(SimGameState __instance)
+            {
+                List<Pilot> list = new List<Pilot>(__instance.PilotRoster);
+                list.Add(__instance.Commander);
+                foreach (Pilot pilot in list)
+                {    
+                    pilot.StatCollection.AddStatistic<int>("TotalMechKills", pilot.pilotDef.MechKills);
+                    pilot.StatCollection.AddStatistic<int>("TotalOtherKills", pilot.pilotDef.OtherKills);
+                    pilot.StatCollection.AddStatistic<int>("TotalKills", pilot.pilotDef.MechKills + pilot.pilotDef.OtherKills);
+                    pilot.StatCollection.AddStatistic<int>("TotalInjuries", pilot.pilotDef.LifetimeInjuries);
+                }
+            }
+        }
+    }
+}
+
