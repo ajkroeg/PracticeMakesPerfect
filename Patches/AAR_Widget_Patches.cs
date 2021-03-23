fc736db5e2b234f162464c83dbe1ac433dc543e1
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2021-03-23 06:51:18 -0400

    better logging for bonus rep

--- Patches/AAR_Widget_Patches.cs
+++ Patches/AAR_Widget_Patches.cs
@@ -26,12 +26,14 @@ namespace PracticeMakesPerfect.Patches
                 var target = theContract.Override.targetTeam.FactionDef.FactionValue.Name;
 
                 List<string> curPilots = new List<string>();
-                curPilots.Add(GlobalVars.sim.Commander.FetchGUID());
+                
                 foreach (Pilot p in GlobalVars.sim.PilotRoster)
                 {
                     SpecHolder.HolderInstance.AddToMaps(p);
                     curPilots.Add(p.FetchGUID());
                 }
+                curPilots.Add(GlobalVars.sim.Commander.FetchGUID());
+
 
                 var repMultDictionary = new Dictionary<string, float>();
                 
@@ -59,13 +61,12 @@ namespace PracticeMakesPerfect.Patches
                                     }
                                     ModInit.modLog.LogMessage($"repMultDictionary contains {repMult.Key} and {repMult.Value}");
                                 }
-                                
                             }
                         }
                     }
                 }
 
-                var idx = 2;
+                var idx = ___FactionWidgets.Count;
                 foreach (var repMult in repMultDictionary)
                 {
                     SGReputationWidget_Simple component = sim.DataManager
@@ -84,12 +85,20 @@ namespace PracticeMakesPerfect.Patches
                     {
                         repChange = Mathf.RoundToInt( SpecHolder.HolderInstance.emplRep *
                                                          repMultDictionary[faction.FactionValue.Name]);
+                        ModInit.modLog.LogMessage($"Employer {theContract.Override.employerTeam.FactionDef.FactionValue.Name} is reputation gainer: base employer change {SpecHolder.HolderInstance.emplRep}");
                     }
                     else
                     {
-                        repChange = Mathf.RoundToInt(Math.Abs(theContract.TargetReputationResults *
-                                                     repMultDictionary[faction.FactionValue.Name]));
-                        if (repChange <= 1) repChange = theContract.Difficulty + 2; //this is some hacky ugly bullshit for stupid edgecase where player is working for local gov against someone with fully negatived rep.
+                        repChange = Math.Abs(Mathf.RoundToInt(theContract.TargetReputationResults *
+                                                      repMultDictionary[faction.FactionValue.Name]));
+                        ModInit.modLog.LogMessage($"Employer {theContract.Override.employerTeam.FactionDef.FactionValue.Name} is NOT reputation gainer: base target rep change {theContract.TargetReputationResults}");
+
+
+                        if (repChange <= 1)
+                        {
+                            repChange = theContract.Difficulty + 2; //this is some hacky ugly bullshit for stupid edgecase where player is working for local gov against someone with fully negatived rep.
+                            ModInit.modLog.LogMessage($"{faction.Name} rep change would have been <=1, changing to contract difficulty +2: {theContract.Difficulty + 2}");
+                        }
                     }
 
                     __instance.SetWidgetData(idx, faction.FactionValue, repChange, true, false);
@@ -98,7 +107,6 @@ namespace PracticeMakesPerfect.Patches
                     ModInit.modLog.LogMessage($"Reputation with {faction.FactionValue.Name} changed by {repChange}");
 
                     idx += 1;
-
                 }
             }
         }
